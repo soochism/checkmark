e9ae1df56225974df6361ef28b27801ede368ced
Soo Chan An <soochism@umich.edu>
2016-02-23 20:35:58 -0500

    basic login page

--- checkmark/checkmark/ViewController.swift
+++ checkmark/checkmark/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var TommyLoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Actions
 
+    @IBAction func loginTommy(sender: UIButton) {
+        
+    }
 }
 
---
+++ checkmark/checkmark/ViewTwo.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTwo.swift
+//  checkmark
+//
+//  Created by Soo Chan An on 2/23/16.
+//  Copyright (c) 2016 checkmark. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewTwo : UIViewController {
+    
+    
+    override func viewDidLoad() {
+    }
+}
