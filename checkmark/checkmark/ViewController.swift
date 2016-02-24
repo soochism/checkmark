1a6a14da6bef3c17b64a433f847bd9d79575aaa6
Shan-En Liu <Shan-En@0587368629.wireless.umich.net>
2016-02-23 20:13:01 -0500

    finish friendspage

--- checkmark/checkmark/ViewController.swift
+++ checkmark/checkmark/ViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
